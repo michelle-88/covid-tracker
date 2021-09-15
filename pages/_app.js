d10f3be017fad663d9289e72ce929c19d4c561de
Michelle M <53539385+michelle-88@users.noreply.github.com>
2021-09-14 21:02:14 -0400

    added a loader component that displays while API data is loading.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import CustomThemeProvider from '../theme/CustomThemeProvider';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Loader from '../components/Loader';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loaderOpen, setLoaderOpen] = useState(false);
+  const handleOpen = () => {
+    setLoaderOpen(true);
+  };
+  const handleClose = () => {
+    setLoaderOpen(false);
+  };
+
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -11,6 +22,18 @@ export default function MyApp({ Component, pageProps }) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleOpen);
+    router.events.on('routeChangeComplete', handleClose);
+    router.events.on('routeChangeError', handleClose);
+
+    return () => {
+      router.events.off('routeChangeStart', handleOpen);
+      router.events.off('routeChangeComplete', handleClose);
+      router.events.off('routeChangeError', handleClose);
+    };
+  }, [router]);
 
   return (
     <React.Fragment>
@@ -23,7 +46,11 @@ export default function MyApp({ Component, pageProps }) {
       <CustomThemeProvider>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        {loaderOpen ? (
+          <Loader open={loaderOpen} />
+        ) : (
+          <Component {...pageProps} />
+        )}
       </CustomThemeProvider>
     </React.Fragment>
   );
